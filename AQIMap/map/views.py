d3803d2ad17a0ebfed2d939c498dfdae1af9dd4e
C Hu@ng <1457043769@qq.com>
2022-03-12 17:04:08 +0800

    initial data model

--- AQIMap/map/views.py
+++ AQIMap/map/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+import geoip2.database
+import json
+from map.models import AqiData
 # Create your views here.
+
+
+def index(request):
+    pass
